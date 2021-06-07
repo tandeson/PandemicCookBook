bade8b92e490ce347857748d54f37e1bb6b001c8
Thomas <tandeson@gmail.com>
2021-06-07 08:27:15 -0700

    Switched ingredient.
     * Maple syrup to sugar - it's what I actually used, and I liked the outcome.

--- recipes_for_book_input/083_SourdoughMuffins/recipe_SourdoughDiscardMuffins.py
+++ recipes_for_book_input/083_SourdoughMuffins/recipe_SourdoughDiscardMuffins.py
@@ -35,20 +35,20 @@ def makeRecipe( dirPathRecipe, sharedIngredentList ):
         r.addIngredient('Salt', 0.75, 'teaspoon')
         r.addIngredient('Baking Soda', 1, 'teaspoon')
         r.addIngredient('Cinnamon', 1.5, 'teaspoons')
+        r.addIngredient('Powdered Sugar', 156, 'grams, 1/2 cup')
         
         r.addIngredient('Sourdough Starter', 227, 'grams (1 cup)')
         r.addIngredient('Whole Milk', 57, 'grams (1/4 cup)')
         r.addIngredient('Eggs', 1, 'large')
         r.addIngredient('Unsalted Butter', 57, 'grams, 4 tablespoons, melted')
-        r.addIngredient('Maple Syrup', 156, 'grams, 1/2 cup')
         r.addIngredient('Blueberries', 340, 'grams, 2 cups')
 
         # Add Steps and Notes
         steps = [
             'Preheat oven to 425 deg F. Grease a 12-cup muffin pan.',
             'Combine the dry ingredients in a mixing bowl.',
-            'In a second bowl, beat together the starter, milk, egg, melted butter,'
-            ' and sweetener. Blend the wet ingredients with the dry, taking about 20 '
+            'In a second bowl, beat together the starter, milk, egg and melted butter,'
+            '. Blend the wet ingredients with the dry, taking about 20 '
             'seconds. Gently stir in the blueberries just until blended',
             'Fill the pan cups evenly with the batter.',
             'Bake the muffins for 25 minutes, until a toothpick inserted in the center'
