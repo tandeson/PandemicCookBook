861cd8ccc6861bc75a4d958e262687336fe773e5
Thomas <tandeson@gmail.com>
2021-12-01 09:11:08 -0800

    Added Banana Bites
     * Bilyana tried it, liked it, ate them all- so, in it goes.

---
+++ recipes_for_book_input/096_Banana_Bites/recipe_Banana_coconut_bites.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#*****************************************************************************
+"""
+    Make A recipe
+"""
+#*****************************************************************************
+
+#*  Imports ******************************************************************
+import sys
+
+from scripts.myRecipe import MyRecipe
+from scripts.myRecipe import RecipeStep
+
+#*  Constants ****************************************************************
+# PY-2.10
+# DELIMITER = ","
+
+#*  Class and Function Definitions *******************************************
+
+#=============================================================================
+def makeRecipe( dirPathRecipe, sharedIngredentList ):
+        """
+        Make this specific Recipe
+        """
+        r = MyRecipe('Banana Bites', 'Dessert', sharedIngredentList)
+        r.setPathLoc(dirPathRecipe)
+        r.addPicture('banana_bites', '2021_12_Banana_Bites_1.jpg')
+        r.setPrimaryPicture( 'banana_bites' )
+        r.setRecipeFormat('FANCY_TALL_PIC_OVER_INSTRUCTIONS')
+        
+        #  -- Add Ingredients --
+
+        ## 
+        r.addIngredient('Banana', 1, 'ripe ')
+        r.addIngredient('Oatmeal', 0.5, 'cup, rolled')
+        r.addIngredient('Walnuts', 0.25, 'cup, pieces')
+        
+        # Add Steps and Notes
+        steps = [
+            'Preheat oven to 350 deg F',
+            'In a bowl mix all ingredients.',
+            'Fill a baking sheeting sheet with 1 tablespoon sized scopes of batter.',
+            'Bake for 15 minutes.',
+             ]
+        
+        for s in steps:
+            r.addStep( RecipeStep( s ) )
+        
+        
+        # Return this back.
+        return r
+
+#=============================================================================
+def main(argv=None):
+    """
+    Description of program.
+    """
+    pass
+
+
+#*  Main Code Path ***********************************************************
+
+if __name__ == "__main__":
+    # Exit code is main() return value
+    # (see http://www.artima.com/weblogs/viewpost.jsp?thread=4829)
+    sys.exit(main())
+
+
+#*****************************************************************************
+#*****************************************************************************
